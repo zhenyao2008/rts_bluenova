59b34788cde12b7299018731a1e4a7a30d22c41c
应彧刚 <yingyugang@yingyugangdeMacBook-Pro.local>
2017-09-25 15:15:40 +0900

    amend commen.

--- Assets/Moba/Scripts/Core/UnitProperties.cs
+++ Assets/Moba/Scripts/Core/UnitProperties.cs
@@ -2,23 +2,34 @@
 using System.Collections;
 
 [ExecuteInEditMode]
-public class UnitProperties : MonoBehaviour {
+public class UnitProperties : MonoBehaviour
+{
 
 	public bool load;
 	public UnitAttribute loadTarget;
+	//单位名称
+	public string unitName;
+	//存放路径
+	public string prefabPath;
+	//建造时间
+	public int buildDuration;
+	//最小伤害
+	public int minDamage = -1;
+	//最大伤害
+	public int maxDamage = -1;
+	//攻击类型
+	public AttackType attackType = AttackType.Normal;
+	//攻击间隔
+	public float attackInterval;
+	//攻击距离
+	public float attackRange;
+	//最小攻击距离
+	public float minAttackRange;
+	//是否近战
+	public bool isMelee;
+	//基础生命
+	public int baseHealth = -1;
 
-	public string unitName;//单位名称
-	public string prefabPath;//存放路径
-	public int buildDuration;//建造时间
-	public int minDamage = -1;//最小伤害
-	public int maxDamage = -1;//最大伤害
-	public AttackType attackType = AttackType.Normal;//攻击类型
-	public float attackInterval;//攻击间隔
-	public float attackRange;//攻击距离
-	public float minAttackRange;//最小攻击距离
-
-	public bool isMelee;//是否近战
-	public int baseHealth = -1;//基础生命
 	public int armor = -1;
 	public ArmorType armorType = ArmorType.Heavy;
 	public string skillInfo;
@@ -32,34 +43,35 @@ public class UnitProperties : MonoBehaviour {
 	public int exp = -1;
 	public int levelUpExp = -1;
 	public int level = -1;
-	// Update is called once per frame
-	void Update () {
-		if(load){
-			UnitAttribute ua = GetComponent<UnitAttribute>();
-			if(loadTarget)
+
+	void Update ()
+	{
+		if (load) {
+			UnitAttribute ua = GetComponent<UnitAttribute> ();
+			if (loadTarget)
 				ua = loadTarget;
-			unitName =ua.unitName;
-			buildDuration=ua.buildDuration;//建造时间
-			minDamage =ua.minDamage;
-			maxDamage =ua.maxDamage;
-			attackType =ua.attackType;
-			attackInterval=ua.attackInterval;//攻击间隔
-			attackRange=ua.attackRange;//攻击距离
-			isMelee=ua.isMelee;
-			baseHealth=ua.baseHealth;
-			armor =ua.armor;
-			armorType =ua.armorType;
-			skillInfo=ua.skillInfo;
-			killPrice =ua.killPrice;
-			healthRecover =ua.healthRecover;
-			mana=ua.mana;
-			manaRecover=ua.manaRecover;
-			baseDamage=ua.baseDamage;
-			currentHealth =ua.currentHealth;
-			maxHealth=ua.maxHealth;
-			exp =ua.exp;
-			levelUpExp =ua.levelUpExp;
-			level =ua.level;
+			unitName = ua.unitName;
+			buildDuration = ua.buildDuration;//建造时间
+			minDamage = ua.minDamage;
+			maxDamage = ua.maxDamage;
+			attackType = ua.attackType;
+			attackInterval = ua.attackInterval;//攻击间隔
+			attackRange = ua.attackRange;//攻击距离
+			isMelee = ua.isMelee;
+			baseHealth = ua.baseHealth;
+			armor = ua.armor;
+			armorType = ua.armorType;
+			skillInfo = ua.skillInfo;
+			killPrice = ua.killPrice;
+			healthRecover = ua.healthRecover;
+			mana = ua.mana;
+			manaRecover = ua.manaRecover;
+			baseDamage = ua.baseDamage;
+			currentHealth = ua.currentHealth;
+			maxHealth = ua.maxHealth;
+			exp = ua.exp;
+			levelUpExp = ua.levelUpExp;
+			level = ua.level;
 			load = false;
 			loadTarget = null;
 		}
