52ee361dc81e9cbfbceb42cfdf6dc46af338306c
yingyugang <yingyugang@gmail.com>
2018-10-24 19:17:51 +0900

    Init config manage.

---
+++ Assets/Moba/Scripts/Config/ConfigUtility.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ConfigUtility {
+
+	public static SystemConfig systemConfig;
+
+	const string SYSTEM_CONFIG_PATH = "configs/SystemConfig";
+
+	static ConfigUtility(){
+		systemConfig = JsonUtility.FromJson<SystemConfig> (SYSTEM_CONFIG_PATH);
+	}
+}
+
+[System.Serializable]
+public class SystemConfig{
+	//今度コーンを増える時間帯
+	public float moneyPlusInterval;
+
+	public int moneyPlusPer; 
+
+}
