5367d11b24db90f7f43a87711e06a63856f6c0e8
yingyugang <yingyugang@gmail.com>
2018-10-30 21:19:32 +0900

    load unit attribute from prefab.

--- Assets/Moba/Scripts/Localization/LocalizationManager.cs
+++ Assets/Moba/Scripts/Localization/LocalizationManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 namespace BlueNoah.Localzation
@@ -67,21 +68,55 @@ namespace BlueNoah.Localzation
             List<UnitAttributeEntity> unitAttributeList = new List<UnitAttributeEntity>();
 
             string path = UnityEditor.AssetDatabase.GetAssetPath(UnityEditor.Selection.activeObject);
-            Debug.Log(path);
+            //Debug.Log(path);
             string[] prefabs = UnityEditor.AssetDatabase.FindAssets("t:GameObject", new string[1]{"Assets/Moba/Prefabs/Soldiers"});
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("id,resourceName,buildCorn,unitName,buildDuration,minDamage,maxDamage,attackType,attackInterval,attackRange,isMelee,baseHealth,armor,armorType,skillInfo,killPrice,healthRecover,mana," +
+                                     "manaRecover,maxHealth,killExp,levelUpExp,corn");
             for (int i = 0; i < prefabs.Length; i++)
             {
-                UnitAttribute unitAttribute = UnityEditor.AssetDatabase.LoadAssetAtPath<GameObject>(prefabs[i]).GetComponent<UnitAttribute>();// UnityEditor.Selection.gameObjects[i].GetComponent<UnitAttribute>();
-                unitAttribute.unitId = i;
+                string prefabPath = UnityEditor.AssetDatabase.GUIDToAssetPath(prefabs[i]);
+                Debug.Log(prefabPath);
+                UnitAttribute unitAttribute = UnityEditor.AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath).GetComponent<UnitAttribute>();
+                unitAttribute.unitId = 10000 + i;
+                string unitAttributeString = UnitAttributeToString(unitAttribute,prefabPath.Replace("Assets/Moba/Prefabs/", "").Replace(".prefab",""));
+                stringBuilder.AppendLine(unitAttributeString);
                 UnitAttributeEntity unitAttributeEntity = UnitAttributeEntity.CoverTo(unitAttribute);
                 if (unitAttributeEntity != null)
                     unitAttributeList.Add(unitAttributeEntity);
-
             }
             UnitAttributeGroup unitAttributeGroup = new UnitAttributeGroup();
             unitAttributeGroup.unitAttributes = unitAttributeList.ToArray();
-            Debug.Log(JsonUtility.ToJson(unitAttributeGroup, true));
+            Debug.Log(stringBuilder.ToString());
 #endif
+        }
+
+        string UnitAttributeToString(UnitAttribute unitAttribute,string path){
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(unitAttribute.unitId + ",");
+            stringBuilder.Append(path + ",");
+            stringBuilder.Append(unitAttribute.buildCorn + ",");
+            stringBuilder.Append(unitAttribute.unitName + ",");
+            stringBuilder.Append(unitAttribute.buildDuration + ",");
+            stringBuilder.Append(unitAttribute.minDamage + ",");
+            stringBuilder.Append(unitAttribute.maxDamage + ",");
+            stringBuilder.Append(unitAttribute.attackType + ",");
+            stringBuilder.Append(unitAttribute.attackInterval + ",");
+            stringBuilder.Append(unitAttribute.attackRange + ",");
+            stringBuilder.Append(unitAttribute.isMelee + ",");
+            stringBuilder.Append(unitAttribute.baseHealth + ",");
+            stringBuilder.Append(unitAttribute.armor + ",");
+            stringBuilder.Append(unitAttribute.armorType + ",");
+            stringBuilder.Append(unitAttribute.skillInfo + ",");
+            stringBuilder.Append(unitAttribute.killPrice + ",");
+            stringBuilder.Append(unitAttribute.healthRecover + ",");
+            stringBuilder.Append(unitAttribute.mana + ",");
+            stringBuilder.Append(unitAttribute.manaRecover + ",");
+            stringBuilder.Append(unitAttribute.maxHealth + ",");
+            stringBuilder.Append(unitAttribute.killExp + ",");
+            stringBuilder.Append(unitAttribute.levelUpExp + ",");
+            stringBuilder.Append(unitAttribute.corn);
+            return stringBuilder.ToString();
         }
     }
 
