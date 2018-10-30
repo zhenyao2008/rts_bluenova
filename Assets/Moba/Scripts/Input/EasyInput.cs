1e6ca7d78ffd3b957d8a1281c5cb19dd23eb937d
yingyugang <yingyugang@gmail.com>
2018-10-30 21:18:19 +0900

    amend easyinput.

--- Assets/Moba/Scripts/Input/EasyInput.cs
+++ Assets/Moba/Scripts/Input/EasyInput.cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.EventSystems;
 
 namespace BlueNoah.Event
 {
-    public enum TouchType { TouchBegin, TouchEnd, Touch, Click, LongPress, PinchBegin, Pinch, PinchEnd };
+    public enum TouchType { TouchBegin, TouchEnd, Touch, Click, LongPress, TwoFingerBegin, TwoFinger, TwoFingerEnd};
 
     public class EventData
     {
@@ -14,10 +13,12 @@ namespace BlueNoah.Event
         public Vector3 deltaTouchPos0;
         public float touchStartTime0;
 
+        public Vector3 touchStartPos1;
         public Vector3 touchPos1;
         public Vector3 deltaTouchPos1;
         public float touchStartTime1;
 
+        public float deltaAngle;
         public float pinchDistance;
         public float deltaPinchDistance;
     }
@@ -41,9 +42,9 @@ namespace BlueNoah.Event
             touchActionDic.Add(TouchType.Touch, new List<UnityAction<EventData>>());
             touchActionDic.Add(TouchType.Click, new List<UnityAction<EventData>>());
             touchActionDic.Add(TouchType.LongPress, new List<UnityAction<EventData>>());
-            touchActionDic.Add(TouchType.PinchBegin, new List<UnityAction<EventData>>());
-            touchActionDic.Add(TouchType.Pinch, new List<UnityAction<EventData>>());
-            touchActionDic.Add(TouchType.PinchEnd, new List<UnityAction<EventData>>());
+            touchActionDic.Add(TouchType.TwoFingerBegin, new List<UnityAction<EventData>>());
+            touchActionDic.Add(TouchType.TwoFinger, new List<UnityAction<EventData>>());
+            touchActionDic.Add(TouchType.TwoFingerEnd, new List<UnityAction<EventData>>());
 
             lateTouchActionDic = new Dictionary<TouchType, List<UnityAction<EventData>>>();
             lateTouchActionDic.Add(TouchType.TouchBegin, new List<UnityAction<EventData>>());
@@ -51,9 +52,9 @@ namespace BlueNoah.Event
             lateTouchActionDic.Add(TouchType.Touch, new List<UnityAction<EventData>>());
             lateTouchActionDic.Add(TouchType.Click, new List<UnityAction<EventData>>());
             lateTouchActionDic.Add(TouchType.LongPress, new List<UnityAction<EventData>>());
-            lateTouchActionDic.Add(TouchType.PinchBegin, new List<UnityAction<EventData>>());
-            lateTouchActionDic.Add(TouchType.Pinch, new List<UnityAction<EventData>>());
-            lateTouchActionDic.Add(TouchType.PinchEnd, new List<UnityAction<EventData>>());
+            lateTouchActionDic.Add(TouchType.TwoFingerBegin, new List<UnityAction<EventData>>());
+            lateTouchActionDic.Add(TouchType.TwoFinger, new List<UnityAction<EventData>>());
+            lateTouchActionDic.Add(TouchType.TwoFingerEnd, new List<UnityAction<EventData>>());
         }
 
         public void RemoveAllListener(TouchType touchType)
@@ -76,18 +77,6 @@ namespace BlueNoah.Event
 
         bool GetTouchDown()
         {
-            if(EventSystem.current != null)
-            {
-                if ( EventSystem.current.IsPointerOverGameObject() || EventSystem.current.IsPointerOverGameObject(1) || EventSystem.current.IsPointerOverGameObject(0))
-                {
-                    return false;
-                } 
-            }
-            if(UICamera.isOverUI )
-            {
-                return false;
-            }
-
             bool isDown = false;
 #if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBGL
             if (Input.GetMouseButtonDown(0))
@@ -173,56 +162,57 @@ namespace BlueNoah.Event
 
         bool GetTwoFingerBegin()
         {
-            bool isPinchDown = false;
+            bool isTwoFingerDown = false;
 #if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBGL
             if ((Input.GetKeyDown(KeyCode.Q) && Input.GetMouseButton(0)) || (Input.GetKey(KeyCode.Q) && Input.GetMouseButtonDown(0))
                || (Input.GetKeyDown(KeyCode.Q) && Input.GetMouseButtonDown(0)))
             {
-                isPinchDown = true;
+                isTwoFingerDown = true;
+
             }
 #else
             if ((Input.touches.Length == 2 && Input.touches[1].phase == TouchPhase.Began && Input.touches[0].phase != TouchPhase.Ended && Input.touches[0].phase != TouchPhase.Canceled) 
                 || (Input.touches.Length == 2 && Input.touches[0].phase == TouchPhase.Began && Input.touches[1].phase != TouchPhase.Ended && Input.touches[1].phase != TouchPhase.Canceled))
             {
-                isPinchDown =  true;
+                isTwoFingerDown =  true;
             }
 #endif
-            return isPinchDown;
+            return isTwoFingerDown;
         }
 
         bool GetTwoFinger()
         {
-            bool isPinch = false;
+            bool isTwoFinger = false;
 #if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBGL
             if (Input.GetKey(KeyCode.Q) && Input.GetMouseButton(0))
             {
-                isPinch = true;
+                isTwoFinger = true;
             }
 #else
             if(Input.touches.Length == 2 && (Input.touches[0].phase == TouchPhase.Moved || Input.touches[0].phase == TouchPhase.Stationary) && (Input.touches[1].phase == TouchPhase.Moved || Input.touches[1].phase == TouchPhase.Stationary)){
-                isPinch = true;
+                isTwoFinger = true;
             }
 #endif
-            return isPinch;
+            return isTwoFinger;
         }
 
         bool GetTwoFingerEnd()
         {
-            bool isPinchUp = false;
+            bool isTwoFingerUp = false;
 #if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBGL
             if ((Input.GetKeyUp(KeyCode.Q) && Input.GetMouseButton(0)) || (Input.GetKey(KeyCode.Q) && Input.GetMouseButtonUp(0))
                 || (Input.GetKeyUp(KeyCode.Q) && Input.GetMouseButtonUp(0)))
             {
-                isPinchUp = true;
+                isTwoFingerUp = true;
             }
 #else
             if ((Input.touches.Length == 2 && (Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled)) 
                 || (Input.touches.Length == 2 && (Input.touches[1].phase == TouchPhase.Ended || Input.touches[1].phase == TouchPhase.Canceled)))
             {
-                isPinchUp =  true;
+                isTwoFingerUp =  true;
             }
 #endif
-            return isPinchUp;
+            return isTwoFingerUp;
         }
 
         void OnActions(List<UnityAction<EventData>> unityActions)
@@ -258,17 +248,17 @@ namespace BlueNoah.Event
             if (GetTwoFingerBegin())
             {
                 OnTwoFingerBegin();
-                OnActions(touchActionDic[TouchType.PinchBegin]);
+                OnActions(touchActionDic[TouchType.TwoFingerBegin]);
             }
             if (GetTwoFinger())
             {
                 OnTwoFinger();
-                OnActions(touchActionDic[TouchType.Pinch]);
+                OnActions(touchActionDic[TouchType.TwoFinger]);
             }
             if (GetTwoFingerEnd())
             {
                 OnTwoFingerEnd();
-                OnActions(touchActionDic[TouchType.PinchEnd]);
+                OnActions(touchActionDic[TouchType.TwoFingerEnd]);
             }
         }
 
@@ -295,19 +285,30 @@ namespace BlueNoah.Event
         {
             mEventData.touchPos0 = GetTouchPosition(0);
             mEventData.touchPos1 = GetTouchPosition(1);
+            mEventData.touchStartPos1 = GetTouchPosition(1);
             mEventData.pinchDistance = Vector3.Distance(mEventData.touchPos0, mEventData.touchPos1);
         }
 
         void OnTwoFinger()
         {
             mEventData.deltaTouchPos0 = GetTouchPosition(0) - mEventData.touchPos0;
+            mEventData.deltaTouchPos1 = GetTouchPosition(1) - mEventData.touchPos1;
+            mEventData.deltaAngle = SignedAngle(GetTouchPosition(1) - GetTouchPosition(0), mEventData.touchPos1 - mEventData.touchPos0,new Vector3(0,0,1));
             mEventData.touchPos0 = GetTouchPosition(0);
-            mEventData.deltaTouchPos1 = GetTouchPosition(0) - mEventData.touchPos1;
             mEventData.touchPos1 = GetTouchPosition(1);
             float currentDistance = Vector3.Distance(mEventData.touchPos0, mEventData.touchPos1);
             mEventData.deltaPinchDistance = currentDistance - mEventData.pinchDistance;
             mEventData.pinchDistance = currentDistance;
         }
+
+        float SignedAngle(Vector3 from,Vector3 to,Vector3 axis){
+            Vector3 normalized = from.normalized;
+            Vector3 normalized2 = to.normalized;
+            float num = Mathf.Acos(Mathf.Clamp(Vector3.Dot(normalized, normalized2), -1f, 1f)) * 57.29578f;
+            float num2 = Mathf.Sign(Vector3.Dot(axis, Vector3.Cross(normalized, normalized2)));
+            return num * num2;
+        }
+
 
         void OnTwoFingerEnd()
         {
@@ -348,15 +349,15 @@ namespace BlueNoah.Event
             }
             if (GetTwoFingerBegin())
             {
-                OnActions(lateTouchActionDic[TouchType.PinchBegin]);
+                OnActions(lateTouchActionDic[TouchType.TwoFingerBegin]);
             }
             if (GetTwoFinger())
             {
-                OnActions(lateTouchActionDic[TouchType.Pinch]);
+                OnActions(lateTouchActionDic[TouchType.TwoFinger]);
             }
             if (GetTwoFingerEnd())
             {
-                OnActions(lateTouchActionDic[TouchType.PinchEnd]);
+                OnActions(lateTouchActionDic[TouchType.TwoFingerEnd]);
             }
         }
 
