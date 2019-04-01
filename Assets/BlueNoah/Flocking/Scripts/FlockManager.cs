246f9b3e186c6172aa48e68e5a2f469e39d0be20
yingyugang <yingyugang@gmail.com>
2019-04-01 16:10:04 +0900

    amend Math and Nav.

---
+++ Assets/BlueNoah/Flocking/Scripts/FlockManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BlueNoah.AI.Flocking
+{
+    public class FlockManager : SimpleSingleMonoBehaviour<FlockManager>
+    {
+        //Captain is the flock group center.
+        // public FlockComponent captain;
+        public Transform center;
+        //Menbers of the flock group.
+        public FlockComponent[] members;
+
+        public Vector3 speed = Vector3.forward;
+
+        public float flockPercent;
+
+        public float separationWeight = 0.33f;
+
+        public float centerWeight = 0.33f;
+
+        public float velocityWeight = 0.33f;
+    }
+}
---
+++ Assets/BlueNoah/Framework_Common/Scripts/Math/MathUtility.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BlueNoah.Math
+{
+
+
+    //线性代数。 
+    //線形代数。
+    //Linearalgebra.
+    public static class MathUtility
+    {
+
+        public static bool IsInPlane(Vector3 point, Vector3 p0, Vector3 p1, Vector3 p2)
+        {
+
+
+
+            // Vector3.Cross();
+
+            // float minX = p0.x <= p1.x ? p0.x : p1.x;
+            // float maxX = p0.x > p1.x ? p0.x : p1.x;
+            // float minY = p0.y <= p1.y ? p0.y : p1.y;
+            // float maxY = p0.y > p1.y ? p1.y : p1.y;
+            // float minZ = p0.z <= p1.z ? p0.z : p1.z;
+            // float maxZ = p0.z > p1.z ? p0.z : p1.z;
+
+            // minX = minX <= p2.x ? minX : p2.x;
+            // maxX = maxX > p2.x ? maxX : p2.x;
+            // minY = minY <= p2.y ? minY : p2.y;
+            // maxY = maxY > p2.y ? maxY : p2.y;
+            // minZ = minZ <= p2.z ? minZ : p2.z;
+            // maxZ = maxZ > p2.z ? maxZ : p2.z;
+
+            // Vector3 center = new Vector3((minX + ));
+
+
+            return true;
+        }
+
+
+        //先判断是否在三角形的AABB内。
+        //判断点是否在三角形内。方法1:点在三条边的同一侧；方法2:点所划分的三个三角形的面积之和等于判断三角形的面积。
+        public static bool IsInTriangle(Vector3 point, Vector3 p0, Vector3 p1, Vector3 p2)
+        {
+            //TODO is in AABB.
+
+            Vector3 dir0 = (point - p0).normalized;
+            Vector3 dir1 = (p1 - p0).normalized;
+
+            Vector3 dir2 = (point - p1).normalized;
+            Vector3 dir3 = (p2 - p1).normalized;
+
+            Vector3 dir4 = (point - p2).normalized;
+            Vector3 dir5 = (p0 - p2).normalized;
+
+
+
+
+
+            return true;
+        }
+    }
+}
+
