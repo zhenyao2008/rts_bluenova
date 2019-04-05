c3b3e71afbb7c2282dc437694e318108469578a1
yingyugang <yingyugang@gmail.com>
2019-04-05 18:14:00 +0900

    amend for flock2.

---
+++ Assets/BlueNoah/Flocking/Scripts/FlockComponent2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockComponent2 : MonoBehaviour
+{
+
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        if (collisionInfo.collider)
+        {
+
+        }
+    }
+
+}
