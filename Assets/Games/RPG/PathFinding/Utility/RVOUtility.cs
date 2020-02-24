efab022b7e5fd7b443d7e10539809efa6db81cba
应彧刚 <>
2020-02-24 15:08:19 +0900

    Prepare functions for RVO.

---
+++ Assets/Games/RPG/PathFinding/Utility/RVOUtility.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+namespace RPG
+{
+    public static class RVOUtility  {
+
+    	public static Vector3[] CalculateTangent(Vector3 center,float radius,Vector3 point)
+        {
+           
+            if ((point - center).sqrMagnitude <= radius)
+                return null;
+
+            Vector3[] tangents = new Vector3[2];
+
+            Vector3 interection = (point - center).normalized * radius;
+
+            float angle = Mathf.Acos(radius / Vector3.Distance (point , center));
+
+            Vector3 pos0 = new Quaternion(0, Mathf.Sin(angle /2f), 0, Mathf.Cos(angle/2f)) * interection + center;
+
+            Vector3 pos1 = new Quaternion(0, Mathf.Sin(-angle / 2f), 0, Mathf.Cos(-angle/2f)) * interection + center;
+
+            tangents[0] = pos0;
+
+            tangents[1] = pos1;
+
+            return tangents;
+        }
+    }
+}
---
+++ Assets/Games/RPG/Test/TestTengent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace RPG
+{
+
+    public class TestTengent : MonoBehaviour {
+
+        public Transform circle;
+
+        public Transform point;
+
+        public float radius;
+
+
+        private void OnDrawGizmos()
+        {
+            if (circle && point)
+            {
+                Vector3[] interections = RVOUtility.CalculateTangent(circle.position,radius,point.position);
+                if (interections != null)
+                {
+                    Gizmos.DrawWireSphere(circle.position,radius);
+                    Gizmos.DrawWireSphere(point.position,0.5f);
+                    Gizmos.DrawWireSphere(interections[0], 0.5f);
+                    Gizmos.DrawLine(point.position,interections[0] );
+                    Gizmos.DrawWireSphere(interections[1], 0.5f);
+                    Gizmos.DrawLine(point.position,interections[1] );
+                }
+            }
+        }
+
+    }
+}
+
